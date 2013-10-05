469bee1e5110b85f0b7af3b7215ab451987f4f43
João Pedro <joopeeds@gmail.com>
2013-10-05 17:03:03 -0300

    Server updated

--- server/main.py
+++ server/main.py
@@ -25,216 +25,58 @@ class Usuario(db.Model):
     email = db.EmailProperty(required=True)
     setor = db.StringProperty(required=True)
 
-
-class Pregao(db.Model):
-    detalhes = db.StringProperty(required=True)
-    data = db.DateTimeProperty(required=True)
-
-
-
 class Pedido(db.Model):
     demandante = db.StringProperty(required=True)
-    data_entrada = db.StringProperty(required=True) #db.DateTimeProperty(auto_now_add=True)
+    data_entrada = db.DateTimeProperty(required=True)
     descricao = db.StringProperty(required=True)
     numero = db.StringProperty(required=True)
     email_demandante = db.StringProperty(required=True)
     local = db.StringProperty(default = "PRA/Gabinete")
 
-    legalidade_choices = (
-            "indefinido",
-            "legal",
-            "ilegal"
-        )
+    historico_data = db.ListProperty() 
+    historico_info = db.StringListProperty() 
 
-    legalidade = db.StringProperty(
-        default = "indefinido",
-        choices = set(legalidade_choices)
-    )
+    legalidade_parecer = db.BooleanProperty()
+    legalidade_data_envio = db.DateTimeProperty()
+    legalidade_data_retorno = db.DateTimeProperty()    
+
+    autorizacao_parecer = db.BooleanProperty()
+
+    corretude_descricao = db.BooleanProperty()
+    corretude_quantitativo = db.BooleanProperty()
+    corretude_cotacao = db.BooleanProperty()
+    corretude_data = db.DateTimeProperty()  
     
-
-    autorizacao_choices = (
-            "indefinido",
-            "autorizado",
-            "nao autorizado"
-        )
-
-    autorizacao = db.StringProperty(
-        default = "indefinido",
-        choices = set(autorizacao_choices)
-    )
-
-    conferencia_choices = (
-            "indefinido",
-            "incompleto",
-            "completo"
-    )
-
-    conferencia = db.StringProperty(
-        default = "indefinido",
-        choices = set(conferencia_choices)
-    )
-
-    minuta_choices = (
-            "indefinido",
-            "elaborado"
-        )
-
-    minuta = db.StringProperty(
-        default = "indefinido",
-        choices = set(minuta_choices)
-    )
-
-
-    legalidade_materiais_choices = (
-            "indefinido",
-            "legal",
-            "ilegal"
-        )
-
-    legalidade_materiais = db.StringProperty(
-        default = "indefinido",
-        choices = set(legalidade_materiais_choices)
-    )
+    #Listas em que um indice representa um ciclo completo de elaboracao da minuta
+    minuta_parecer = db.ListProperty()
+    minuta_data_inicio = db.ListProperty()     
+    minuta_data_envio = db.ListProperty()
+    minuta_data_retorno = db.ListProperty()
     
-    pregao_choices = (
-            "indefinido",
-            "informado no SIASE e Comprasnet",
-            "Edital publicado",
-            "Data marcada",
-            "Realizado"
-        )
+    #ciclo do pregao
+    pregao_parecer = db.ListProperty()
+    pregao_data = db.ListProperty()
+    pregao_numero = db.ListProperty()
+    licitacao_data = db.ListProperty()
 
-    pregao = db.StringProperty(
-        default = "indefinido",
-        choices = set(pregao_choices)
-    )
+    adjudicacao_data = db.DateTimeProperty()  
+    homologacao_data = db.DateTimeProperty()  
+    publicacao_data = db.DateTimeProperty()  
 
-    licitacao_choices = (
-            "indefinido",
-            "concluida",
-            "em avaliacao"
-        )
+    detalhamento_parecer = db.BooleanProperty()  
+    detalhamento_data = db.DateTimeProperty()  
 
-    licitacao = db.StringProperty(
-        default = "indefinido",
-        choices = set(licitacao_choices)
-    )
-
-    adjudicacao_choices = (
-            "indefinido",
-            "termo elaborado"
-        )
-
-    adjudicacao = db.StringProperty(
-        default = "indefinido",
-        choices = set(adjudicacao_choices)
-    )
-
-    homologacao_choices = (
-        "indefinido", 
-            "homologado",
-            "nao homologado"
-        )
-
-    homologacao = db.StringProperty(
-        default = "indefinido",
-        choices = set(homologacao_choices)
-    )    
-
-    publicacao_choices = (
-            "indefinido",
-            "publicado"
-        )
-
-    publicacao = db.StringProperty(
-        default = "indefinido",
-        choices = set(publicacao_choices)
-    )
-
-    minuta_empenho_choices = (
-            "indefinido",
-            "elaborada"
-        )
-
-    minuta_empenho = db.StringProperty(
-        default = "indefinido",
-        choices = set(minuta_empenho_choices)
-    )
-
-    detalhamento_choices = (
-            "indefinido",
-            "detalhado",
-            "no SEPLAN"
-        )
-
-    detalhamento = db.StringProperty(
-        default = "indefinido",
-        choices = set(detalhamento_choices)
-    )
-
-    empenho_choices = (
-            "indefinido",
-            "empenhado",
-            "recebido"
-        )
+    empenho_data = db.DateTimeProperty() 
     
-    empenho = db.StringProperty(
-        default = "indefinido",
-        choices = set(empenho_choices)
-    )
+    nota_almoxarifado_data = db.DateTimeProperty() 
+    
+    patrimonio_data = db.DateTimeProperty() 
+    
+    nota_contabilidade_data = db.DateTimeProperty() 
+    
+    liquidacao_data = db.DateTimeProperty() 
 
-    nota_almoxarifado_choices = (
-            "indefinido",
-            "enviado"
-        )
-
-    nota_almoxarifado = db.StringProperty(
-        default = "indefinido",
-        choices = set( nota_almoxarifado_choices)
-    )
-
-    tombamento_choices = (
-            "indefinido",
-        "tombado"
-        )
-
-    tombamento = db.StringProperty(
-        default = "indefinido",
-        choices = set( tombamento_choices)
-    )
-
-    nota_contabilidade_choices = (
-            "indefinido",
-            "enviado"
-        )
-
-    nota_contabilidade = db.StringProperty(
-        default = "indefinido",
-        choices = set(nota_contabilidade_choices )
-    )
-
-    liquidacao_choices = (
-            "indefinido",
-        "servico atestado", 
-            "liquidado"
-        )
-     
-    liquidacao = db.StringProperty(
-        default = "indefinido",
-        choices = set(liquidacao_choices )
-    )
-
-    pagamento_choices = (
-            "indefinido",
-            "pago"
-        )
-
-    pagamento = db.StringProperty(
-        default = "indefinido",
-        choices = set(pagamento_choices)
-    )
-
-
+    pagamento_data = db.DateTimeProperty() 
 
 
 
@@ -276,101 +118,8 @@ users.User("daltonserey@gmail.com"): [1, 21]
     
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    
-        """user = users.get_current_user()
-
-        if user:
-            greeting = ('Seja bem vindo ao sistema PRA, %s! (<a href="%s">sign out</a>)' %
-                        (user.nickname(), users.create_logout_url('/')))
-        if user in users_permission:
-         for perm in users_permission[user]:
-         greeting += ('<br><a href="%s">%s</a>' % (perm, perm))
-        else:
-        greeting += "<br>Você não tem nenhuma permissão<br>"
-        else:
-            greeting = ('<a href="%s">Sign in or register</a>.' %
-                        users.create_login_url('/'))
-"""
-        greeting = """
-<!doctype html>
-<!-- The DOCTYPE declaration above will set the     -->
-<!-- browser's rendering engine into                -->
-<!-- "Standards Mode". Replacing this declaration   -->
-<!-- with a "Quirks Mode" doctype is not supported. -->
-
-<html>
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-
-    <!--                                                               -->
-    <!-- Consider inlining CSS to reduce the number of requested files -->
-    <!--                                                               -->
-    <link type="text/css" rel="stylesheet" href="stylesheets/StockWatcher.css">
-
-    <!--                                           -->
-    <!-- Any title is fine                         -->
-    <!--                                           -->
-     <title>SISTEMA PRA@UFCG</title>
-    
-    <!--                                           -->
-    <!-- This script loads your compiled module.   -->
-    <!-- If you add any GWT meta tags, they must   -->
-    <!-- be added before this line.                -->
-    <!--                                           -->
-    <script type="text/javascript" language="javascript" src="js/stockwatcher.nocache.js"></script>
-  </head>
-
-  <!--                                           -->
-  <!-- The body can have arbitrary html, or      -->
-  <!-- you can leave the body empty if you want  -->
-  <!-- to create a completely dynamic UI.        -->
-  <!--                                           -->
-  <body>
-
-    <!-- OPTIONAL: include this if you want history support -->
-    <iframe src="javascript:''" id="__gwt_historyFrame" tabIndex='-1' style="position:absolute;width:0;height:0;border:0"></iframe>
-    
-    <!-- RECOMMENDED if your web app will not function without JavaScript enabled -->
-    <noscript>
-      <div style="width: 22em; position: absolute; left: 50%; margin-left: -11em; color: red; background-color: white; border: 1px solid red; padding: 4px; font-family: sans-serif">
-        Your web browser must have JavaScript enabled
-        in order for this application to display correctly.
-      </div>
-    </noscript>
-
-    
-<div id="top_bar">
-  <h2 style="padding-left: 1%"><img src="images/spra-ufcg-branco.svg" width="10%"></h2>
-    <div id="user_on_top_bar">  
-    </div>    
-</div>
-<div id="main"><div align="center" id="main_top"></div><div id="main_bottom"></div></div>
-<div id="bottom_bar"><div id="bottom_bar_in">Pr&oacute;-Reitoria de Gest&atilde;o Administrativo-Financeira @ Universidade Federal de Campina Grande - 2013</div></div>
-    
-  </body>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-    
-        self.response.out.write('<html><body>%s</body></html>' % greeting)
+        body = open("client_js/base.html").read()
+        self.response.out.write('<html><body>%s</body></html>' % body)
 
 
 
@@ -450,6 +199,52 @@ class ListaPedido(webapp2.RequestHandler):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+#Fazendo
+class GetPedido(webapp2.RequestHandler):
+    def get(self):
+            query = self.request.get("q")
+            if query:
+                dic = {}
+                for pedido in db.GqlQuery("SELECT * FROM Pedido WHERE numero in ('"+query+"')"):
+                    dic = {
+                        "numero": pedido.numero,
+                        "demandante": pedido.demandante,
+                        "descricao": pedido.descricao,
+                        "data_entrada": pedido.data_entrada,
+                        "email_demandante": pedido.email_demandante,
+                        "local": pedido.local,
+                        "legalidade": { "status": pedido.legalidade, "choices": pedido.legalidade_choices},
+                        "autorizacao": {"status":pedido.autorizacao, "choices":pedido.autorizacao_choices},
+                        "conferencia": {"status":pedido.conferencia, "choices":pedido.conferencia_choices}, 
+                        "minuta":  {"status":pedido.minuta, "choices":pedido.minuta_choices},
+                        "legalidade_materiais":  {"status":pedido.legalidade_materiais, "choices":pedido.legalidade_materiais_choices},
+                        "pregao":  {"status":pedido.pregao, "choices":pedido.pregao_choices},
+                        "licitacao":  {"status":pedido.licitacao, "choices":pedido.licitacao_choices},
+                        "adjudicacao":  {"status":pedido.adjudicacao, "choices":pedido.adjudicacao_choices},
+                        "homologacao":  {"status":pedido.homologacao, "choices":pedido.homologacao_choices},
+                        "publicacao":  {"status":pedido.publicacao, "choices":pedido.publicacao_choices},
+                        "minuta_empenho":  {"status":pedido.minuta_empenho, "choices":pedido.minuta_empenho_choices},
+                        "detalhamento":  {"status":pedido.detalhamento, "choices":pedido.detalhamento_choices},
+                        "empenho":  {"status":pedido.empenho, "choices":pedido.empenho_choices},
+                        "liquidacao":  {"status":pedido.liquidacao, "choices":pedido.liquidacao_choices},
+                        "pagamento":  {"status":pedido.pagamento, "choices":pedido.pagamento_choices}                           
+                        }
+
+                self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
+                self.response.out.write(json.dumps(dict({'status':'Connected'}.items() + dic.items()), indent=2)) 
+ 
 
 
 class ListaPedidoForTable(webapp2.RequestHandler):
